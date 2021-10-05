da1faec9774a677f62690e635e2ef82245f3e65c
Laurie Roy <laurieroy.dev@gmail.com>
2021-10-05 13:03:22 -0700

    Add carrierwave, S3, fog-aws (#1)

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-# YelpDemo - WIP
+# Restaurant Review - WIP
 
-This is clone of yelp. I updated a Rails 4 tutorial. It is built using Ruby 2.7.2, Rails 6.1.4.1, postgreSql 
+This is clone of yelp, reviewing restaurants only. I loosely followed a Rails 4 tutorial. It is built using Ruby 2.7.2, Rails 6.1.4.1, and postgreSql. 
 
 [See Demo](https://laurie-yelpclone.herokuapp.com/)
 
-UI: Bootstrap5
+- UI: Bootstrap5
 <!-- Auth: Devise -->
+- Upload image using carrierwave, storage is on AWS S3
+
+-- TODO: upload images
 
 Acknowledgment: BaseRails.com
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,16 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    # aws_access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),                        # required unless using use_iam_profile
+    # aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),                        # required unless using use_iam_profile
+    use_iam_profile:       true,                         # optional, defaults to false
+  }
+	config.fog_directory  = 'yelpclone-lr'  
+  # add in region if switch from default
+  
+	# require 'carrierwave/orm/activerecord'
+	# required
+  # For an application which utilizes multiple servers but does not need caches persisted across requests,
+  # uncomment the line :file instead of the default :storage.  Otherwise, it will use AWS as the temp cache store.
+  # config.cache_storage = :file
+end
